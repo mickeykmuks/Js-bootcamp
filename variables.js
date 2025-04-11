096bc1a27a3f89db6da8d9831dc6dc446bff609b
mickeykmuks <60361983+mickeykmuks@users.noreply.github.com>
2025-04-11 07:38:54 +0000

    variables - done

---
+++ variables.js
@@ -0,0 +1,28 @@
+// Exercise 1:  variables
+var name = "Mickey";
+var age = 31;
+var student = true;
+
+console.log("My name is "+ name+ ", and I am "+ age + " years old. Am I a student? "+ student);
+
+// Exercise 2:  variables
+var favoriteColor = "blue";
+console.log(favoriteColor);
+
+favoriteColor = "green";
+console.log(favoriteColor);
+
+// Exercise 3:  Arithmetic
+var num1 = 5;
+var num2 = 10;
+
+var sum = num1 + num2;
+var diff = num1 - num2;
+var prod = num1 * num2;
+var quo = num1 / num2;
+
+console.log(num1 +" + "+ num2 +" = " + sum)
+console.log(num1 +" - "+ num2 +" = " + diff)
+console.log(num1 +" x "+ num2 +" = " + prod)
+console.log(num1 +" / "+ num2 +" = " + quo)
+
